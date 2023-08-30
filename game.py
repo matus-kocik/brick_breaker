f941897f401dbb465b77f7fc7a964a95fea16755
Matúš Kočik <matuskocik@gmail.com>
2023-08-30 19:08:12 +0200

    create paddle

--- game.py
+++ game.py
@@ -1,13 +1,29 @@
 import pygame
 
-width, height = 800, 600
-win = pygame.display.set_mode((width, height))
+WIDTH, HEIGHT = 800, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Brick Breaker")
 
 FPS = 60
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 15
 
-def draw (win):
+
+class Paddle:
+    VEL = 5
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))   
+
+def draw (win, paddle):
     win.fill("white")
+    paddle.draw(win)
     pygame.display.update()
 
 
@@ -18,12 +34,14 @@ def main():
     while run:
         clock.tick(FPS)
         
+        paddle = Paddle(WIDTH / 2 - PADDLE_WIDTH / 2, HEIGHT - PADDLE_HEIGHT - 5, PADDLE_WIDTH, PADDLE_HEIGHT, "black")
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
     
-        draw(win)
+        draw(win, paddle)
     
     pygame.quit()
     quit()
